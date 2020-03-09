abe3ab44942b048ff754a31880619641ebd684d6
mobei <mobei615@foxmail.com>
2020-03-09 23:17:43 +0800

    和springboot整合

---
+++ shiro-springboot/src/main/java/com/mobei/shiro/service/UserService.java
@@ -0,0 +1,23 @@
+package com.mobei.shiro.service;
+
+import com.mobei.shiro.dao.UserDao;
+import com.mobei.shiro.domain.User;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+import java.util.List;
+
+@Service
+public class UserService {
+
+    @Autowired
+    private UserDao userDao;
+
+    public User findByName(String name) {
+        return this.userDao.findByUsername(name);
+    }
+
+    public List<User> findAll() {
+        return userDao.findAll();
+    }
+}
